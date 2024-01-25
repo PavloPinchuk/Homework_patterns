445405d7a011e6e8cc0e077a5cdf9eee66571397
PavloPinchuk <71881450+PavloPinchuk@users.noreply.github.com>
2024-01-25 16:52:12 +0200

    Initial commit

---
+++ src/Homework1/Task1/Task3_1.java
@@ -0,0 +1,9 @@
+package Homework1.Task1;
+
+public class Task3_1 {
+    public static void main(String[] args) {
+        CarSimulator carSimulator = new CarSimulator();
+        carSimulator.buildCar(550, 3.9f, 4, 30, "BMW X5");
+        System.out.println(carSimulator);
+    }
+}
---
+++ src/Homework1/Task2/Product1.java
@@ -0,0 +1,8 @@
+package Homework1.Task2;
+
+public class Product1 implements Product{
+    @Override
+    public void doSomething() {
+        System.out.println("Product 1 do something");
+    }
+}
